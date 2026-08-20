1eec78b15ffd1b1ed292fbc6f187554503994730
sumanbaskar <suman2862008@gmail.com>
2026-08-20 11:59:45 +0530

    RDBMS PGN 1 1U25IT107

--- solution.sql
+++ solution.sql
@@ -2,7 +2,7 @@ CREATE DATABASE CollegeDB;
 USE CollegeDB;
 CREATE TABLE Department (
 DepartmentID NUMBER(5) PRIMARY KEY,
-DepartmentName VARCHAR(20) NOT NULL,
-HOD VARCHAR(20) NOT NULL
+DepartmentName VARCHAR(20),
+HOD VARCHAR(20) 
 );
-desc department;
+desc Department;
